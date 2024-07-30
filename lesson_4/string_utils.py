27c0c98afcb9d7bda4f247db9bbffccdd6a4f829
DenFil1981 <filinov@ymail.com>
2024-07-30 23:13:26 +0300

    Changed

--- lesson_4/string_utils.py
+++ lesson_4/string_utils.py
@@ -3,7 +3,7 @@ class StringUtils:
     Класс с полезными утилитами для обработки и анализа строк
     """
     
-    def capitilize(self, string: str) -> str:
+    def capitalize(self, string: str) -> str:
         """
         Принимает на вход текст, делает первую букву заглавной и возвращает этот же текст
         Пример: `capitilize("skypro") -> "Skypro"`
@@ -116,4 +116,4 @@ class StringUtils:
             string += str(lst[i]) + joiner
         
         return string + str(lst[-1])
-Comment
+
